102310303df89db3c634870c8b9abaa6973e2d47
Juan Reyes <juan@inspiringapps.com>
2021-08-24 17:49:17 -0400

    Create LocalFileManager class. Add image saving (caching) to file directory of app

--- CryptoTrackerSwiftUI/Services/CoinImageService.swift
+++ CryptoTrackerSwiftUI/Services/CoinImageService.swift
@@ -15,13 +15,27 @@ class CoinImageService {
     
     private var imageSubscription: AnyCancellable?
     private let coin: CoinModel
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "coin_images"
+    private let imageName: String
     
     init(coin: CoinModel) {
         self.coin = coin
+        self.imageName = coin.id
         getCoinImage()
     }
     
     private func getCoinImage() {
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName) {
+            image = savedImage
+            print("Retrieved image from file manager")
+        } else {
+            downloadCoinImage()
+            print("Retrieved image from API")
+        }
+    }
+    
+    private func downloadCoinImage() {
         print("Downloading Image now!")
         guard let url = URL(string: coin.image) else {
             return
@@ -32,8 +46,10 @@ class CoinImageService {
                 return UIImage(data: data)
             })
             .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
-                self?.image = returnedImage
-                self?.imageSubscription?.cancel()
+                guard let self = self, let downloadedImage = returnedImage else { return }
+                self.image = returnedImage
+                self.imageSubscription?.cancel()
+                self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
             })
     }
     
