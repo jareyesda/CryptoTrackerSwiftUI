bcba52468fd660a171e68cb2f2c75d935a701c79
Juan Reyes <juan@inspiringapps.com>
2021-08-24 16:08:01 -0400

    Xcode built in git test: LocalFileManager.swift added

--- CryptoTrackerSwiftUI/Services/CoinImageService.swift
+++ CryptoTrackerSwiftUI/Services/CoinImageService.swift
@@ -22,6 +22,7 @@ class CoinImageService {
     }
     
     private func getCoinImage() {
+        print("Downloading Image now!")
         guard let url = URL(string: coin.image) else {
             return
         }
---
+++ CryptoTrackerSwiftUI/Utilities/LocalFileManager.swift
@@ -0,0 +1,15 @@
+//
+//  LocalFileManager.swift
+//  CryptoTrackerSwiftUI
+//
+//  Created by Juan Reyes on 8/24/21.
+//
+
+import Foundation
+
+class LocalFileManager {
+    
+    static let instance = LocalFileManager()
+    private init() { }
+    
+}
